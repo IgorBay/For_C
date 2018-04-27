6b596e3e2ea5163358576be195ff4186f7885d11
IgorBay <IgorBay99@mail.ru>
2018-04-27 23:33:58 +0300

    022

---
+++ binbit.c
@@ -0,0 +1,31 @@
+#include <stdio.h>      /* printf */
+#include <string.h>     /* strcat */
+#include <stdlib.h>     /* strtol */
+char *byte_to_binary(int x);
+
+int main(void)
+{
+    {
+
+        char *tmp;
+        char *pbin = "01001001";
+
+        printf("%d\n", strtol(pbin, &tmp, 2));
+    }
+
+    return 0;
+}
+
+char *byte_to_binary(int x)
+{
+    char pbin[9];
+    pbin[0] = '\0';
+
+    int z;
+    for (z = 128; z > 0; z >>= 1)
+    {
+        strcat(pbin, ((x & z) == z) ? "1" : "0");
+    }
+
+    return pbin;
+}
